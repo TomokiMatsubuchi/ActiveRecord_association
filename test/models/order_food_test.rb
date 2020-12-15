0490ad5c8aa261b6b0576a2ae9ac67eab803b8ba
Yohei Goto <yohei_goto@diveintocode.jp>
2020-12-15 11:52:22 +0900

    :monkey: rspec

--- test/models/order_food_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OrderFoodTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/order_information_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OrderInformationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
