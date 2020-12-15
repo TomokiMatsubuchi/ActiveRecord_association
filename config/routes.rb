189f2c4c6e8dcb2137824ca363fba1aaf4da1d5b
Yohei Goto <yohei_goto@diveintocode.jp>
2020-12-15 12:42:51 +0900

    :monkey: to controller spec

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :exercises, only: [] do
+  resources :exercises do
+    collection do
+      get :exercise1
+      get :exercise2
+      get :exercise3
+      get :exercise4
+      get :exercise5
+      get :exercise6
+      get :exercise7
+      get :exercise8
+      get :exercise9
+      get :exercise10
+      get :exercise11
+      get :exercise12
+      get :exercise13
+    end
+  end
 end
