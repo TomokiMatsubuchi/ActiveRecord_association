7324df1cf6137fb70e17873167e417a400248e13
Yohei Goto <yohei_goto@diveintocode.jp>
2021-01-26 19:41:16 +0900

    :monkey: shoulda-matcher

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require File.expand_path('../config/environment', __dir__)
 
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
@@ -18,4 +19,11 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
