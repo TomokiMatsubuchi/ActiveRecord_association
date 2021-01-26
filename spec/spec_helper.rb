5013520f6981c0060b248325a55f12cd5d4f24a9
Yohei Goto <yohei_goto@diveintocode.jp>
2021-01-26 20:43:04 +0900

    :banana: rspec実行するたびにseed読み込んで、rspec終わったらdata消すの保留

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,8 +9,8 @@ RSpec.configure do |config|
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
 
-  config.before(:suite) do
-    require Rails.root.join("db", "seeds")
-  end
+  # config.before(:suite) do
+  #   require Rails.root.join("db", "seeds")
+  # end
 
 end
